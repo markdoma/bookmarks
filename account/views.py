c4423edfca6f5662bce621ebde630656e63aa02d
Mark Jayson Doma <markjaysondoma@Marks-MacBook-Air.local>
2024-03-15 23:00:06 +0800

    add loginform

--- account/views.py
+++ account/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginFrorm
 
-# Create your views here.
+
+def user_login()
