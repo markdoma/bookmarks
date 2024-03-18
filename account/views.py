ff127cb1fff087175d98ab388dc2249c6b0408bd
Mark Jayson Doma <markjaysondoma@Marks-MacBook-Air.local>
2024-03-18 10:44:48 +0800

    add messages

--- account/views.py
+++ account/views.py
@@ -5,6 +5,8 @@ from .forms import LoginFrorm, UserRegistrationForm, \
                     UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
+
 
 
 def user_login(request):
@@ -66,6 +68,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '\
+                             'successfully')
+        else:
+            messages.error(request,'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
