06ab8f8a37a219070fb666221a575cc022728f65
Mark Jayson Doma <markjaysondoma@Marks-MacBook-Air.local>
2024-04-05 13:38:29 +0800

    add redis component

--- images/models.py
+++ images/models.py
@@ -3,10 +3,11 @@ from django.conf import settings
 from django.utils.text import slugify
 from django.urls import reverse
 
+
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             related_name = 'images_created',
-                             on_delete = models.CASCADE)
+                             related_name='images_created',
+                             on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200,
                             blank=True)
@@ -25,14 +26,14 @@ class Image(models.Model):
             models.Index(fields=['-total_likes']),
         ]
         ordering = ['-created']
-    
+
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-            super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
+
     def get_absolute_url(self):
-        return reverse('images:detail', args=[self.id,
-                                              self.slug])
+        return reverse('images:detail', args=[self.id, self.slug])
--- images/views.py
+++ images/views.py
@@ -8,6 +8,14 @@ from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, \
                                     PageNotAnInteger
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+
+# connect to redis
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 @login_required
 def image_create(request):
@@ -35,10 +43,15 @@ def image_create(request):
                    'form': form})
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+     # increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
+    # # increment image ranking by 1
+    # r.zincrby('image_ranking', 1, image.id)
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image,
+                   'total_views': total_views})
 
 @login_required
 @require_POST
